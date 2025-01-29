7821d9631e8644d2920e9fa6444ca6f826efea7b
João Lucas <164104520+Joaolucas013@users.noreply.github.com>
2025-01-29 00:59:32 -0300

    add hr alternativo
    Signed-off-by: João Lucas <164104520+Joaolucas013@users.noreply.github.com>

--- src/main/java/org/example/arm/service/ConsultaService.java
+++ src/main/java/org/example/arm/service/ConsultaService.java
@@ -131,35 +131,70 @@ public class ConsultaService{
             throw new RuntimeException(e);
         }
     }
+//
+//    public List<DadosConsulta> retornaConsultas() {
+//        DadosConsulta dados = null;
+//        String caminho = "C:\\meuscode\\consultasLp2\\consultas.txt";
+//        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(caminho))) {
+//            String linha = bufferedReader.readLine();
+//
+//            while (linha != null) {
+//                linha = linha.replace("Consulta{horario consulta=", "")
+//                        .replace("}", "")
+//                        .replace("paciente=", "")
+//                        .replace("medico=", "");
+//
+//                String[] vetor = linha.split(", ");
+//                String horarioConsulta = vetor[0];
+//                String paciente = vetor[1];
+//                String medico = vetor[2];
+//
+//                String nomePaciente = paciente.split(",")[0].split("=")[1].replace("'", "").trim();
+//                String nomeMedico = medico.trim();
+//                LocalDateTime dataConsulta = LocalDateTime.parse(horarioConsulta);
+//
+//                dados = new DadosConsulta(nomePaciente, nomeMedico, dataConsulta);
+//                linha = bufferedReader.readLine();
+//                dadosConsultas.add(dados);
+//            }
+//        } catch (IOException e) {
+//            throw new RuntimeException();
+//        }
+//        return dadosConsultas;
+//    }
 
     public List<DadosConsulta> retornaConsultas() {
-        DadosConsulta dados = null;
-        String caminho = "C:\\meuscode\\consultasLp2\\consultas.txt";
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(caminho))) {
-            String linha = bufferedReader.readLine();
+        String caminho = "C:\\meuscode\\teste\\consultas.txt";
 
-            while (linha != null) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(caminho))) {
+            String linha =  bufferedReader.readLine();
+
+            while (linha  != null) {
                 linha = linha.replace("Consulta{horario consulta=", "")
                         .replace("}", "")
-                        .replace("paciente=", "")
-                        .replace("medico=", "");
+                        .replace("Paciente{", "")
+                        .replace("Especialidade=", "")
+                        .replace("CRM=", "");
 
                 String[] vetor = linha.split(", ");
-                String horarioConsulta = vetor[0];
-                String paciente = vetor[1];
-                String medico = vetor[2];
 
-                String nomePaciente = paciente.split(",")[0].split("=")[1].replace("'", "").trim();
-                String nomeMedico = medico.trim();
+                if (vetor.length < 4) {
+                    continue;
+                }
+
+                String horarioConsulta = vetor[0].trim();
+                String nomePaciente = vetor[1].split("=")[2].replace("'", "").trim();
+                String nomeMedico = vetor[4].split("=")[1].replace("'", "").trim();
                 LocalDateTime dataConsulta = LocalDateTime.parse(horarioConsulta);
 
-                dados = new DadosConsulta(nomePaciente, nomeMedico, dataConsulta);
-                linha = bufferedReader.readLine();
+                DadosConsulta dados = new DadosConsulta(nomePaciente, nomeMedico, dataConsulta);
                 dadosConsultas.add(dados);
+                linha = bufferedReader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException();
         }
+
         return dadosConsultas;
     }
 
